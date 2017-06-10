ca8f20ef8d1d70de8de4f0598e1689b0d7776e60
yannunes <yannunes@gmail.com>
2017-06-10 14:28:44 +0000

    Index.js is minimalistic. Imports reducers and routing automatically

---
+++ src/app/index.js
@@ -0,0 +1,54 @@
+/**
+ * Import Packages
+ */
+import _ from 'lodash'
+import React from 'react'
+import logger from "redux-logger"
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware } from "redux"
+
+/**
+ * Import Modules
+ */
+import App from './routing/App'
+import Reducers from './reducers/Reducers'
+
+// Needed for onTouchTap. Should go away soon
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+/**
+ * Setting up Reducers, Logger for Developtment, Store and middleware
+ * =============================================================================
+ */
+const reducers = combineReducers(Reducers)
+
+/**
+ * Custom Logger, if needed
+ */
+const myLogger = (store) => (next) => (action) => {
+  console.log("Logged Action ", action)
+  next(action)
+}
+
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(myLogger, logger())
+)
+
+store.subscribe(() => {
+  // console.log('Store updated! ', store.getState())
+})
+
+/**
+ * App Rendering
+ */
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  window.document.getElementById('app')
+)
